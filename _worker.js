b9fe713f7356dcd5e928ee4af9fe4fef1c516850
invesibletint <166238198+invesibletint@users.noreply.github.com>
2024-08-21 15:51:28 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'ae7dbd91-8687-4f45-93c4-ee045926893d';
 
-const พร็อกซีไอพีs = ['167.71.227.92'];
+const พร็อกซีไอพีs = ['159.89.162.250'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
