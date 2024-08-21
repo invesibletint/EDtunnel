5f41d39754bb59018f6724b7553e56e50f418e22
invesibletint <166238198+invesibletint@users.noreply.github.com>
2024-08-21 19:22:10 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,9 +3,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'ae7dbd91-8687-4f45-93c4-ee045926893d';
+let userID = '37343037-d5fe-429d-ba93-d56be74ca004';
 
-const พร็อกซีไอพีs = ['167.71.227.92'];
+const พร็อกซีไอพีs = ['13.201.1.24'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
